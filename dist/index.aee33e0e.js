63035d127a8862d9711897123f2bf2974e0b3fa7
badr ghanbi <bghanbi50@gmail.com>
2022-04-16 07:33:46 +0000

    app parcel for production

---
+++ dist/index.aee33e0e.js
@@ -0,0 +1,2 @@
+const toggler=document.querySelector(".nav__toggle");toggler.addEventListener("click",(()=>{toggler.classList.toggle("moveCircle"),document.body.classList.toggle("bgToWhite"),document.querySelectorAll("h1").forEach((e=>e.classList.toggle("changingHeadingColor"))),document.querySelectorAll("h2").forEach((e=>e.classList.toggle("changingHeadingColor"))),document.querySelectorAll(".items").forEach((e=>e.classList.toggle("magic"))),console.log(document.querySelectorAll(".items"))}));
+//# sourceMappingURL=index.aee33e0e.js.map
