05d8b367a03d5ef1da78a4700a926e4fcfa29bf2
badr ghanbi <bghanbi50@gmail.com>
2022-04-16 07:25:34 +0000

    end

--- app.js
+++ app.js
@@ -3,5 +3,7 @@ toggler.addEventListener('click',()=>{
     toggler.classList.toggle('moveCircle');
     document.body.classList.toggle('bgToWhite');
     document.querySelectorAll('h1').forEach(heading=>heading.classList.toggle('changingHeadingColor'));
-    document.querySelectorAll('.card').forEach(otem=>otem.classList.toggle('cards'));
+    document.querySelectorAll('h2').forEach(heading=>heading.classList.toggle('changingHeadingColor'));
+    document.querySelectorAll('.items').forEach(otem=>otem.classList.toggle('magic'));
+    console.log( document.querySelectorAll('.items'))
 });
